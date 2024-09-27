cda378aad1c27021ebbe3baac738a844773ed8f0
hernei152 <bsilvano@fi.uba.ar>
2024-09-26 22:23:59 -0300

    timeout

--- src/host.py
+++ src/host.py
@@ -1,6 +1,6 @@
 import socket
 import os
-from src.packet import Packet
+from src import Packet
 
 class Host:
     def __init__(self, host, port):
@@ -33,12 +33,21 @@ class Host:
         file_path = os.path.join(target_dir, filename)
 
         with open(file_path, 'wb') as f:
+            current_seq_number = 0
             while True:
                 packet = self.receive_packet()[0]
                 payload = packet.payload
-                print(f"PAYLOAD: {payload} SEQ: {packet.seq_number}")
+
+                if packet.get_seq_number() == current_seq_number:
+                    print("No se recibió el paquete esperado. Reenviando ACK.")
+                    self.send_ack(current_seq_number, client_address=client_address)
+                    continue
+
+                current_seq_number = packet.get_seq_number()
+
                 if payload == b'END':
                     break
+
                 f.write(payload)
                 print(f"FILE: {file_path}")
                 self.send_ack(packet.get_seq_number(), client_address=client_address)
@@ -52,20 +61,22 @@ class Host:
                 print(f"# BYTES: {data.__len__()}")
                 if not data:
                     break
-                if client_address:
-                    self.send_payload(data, seq_number, client_address=client_address)
-                else:
-                    self.send_payload(data, seq_number)
-                
-                packet = self.receive_packet()[0]
-                if not packet.is_ACK and packet.seq_number != seq_number:
-                    print("No se recibió un ACK. Reenviando paquete.")
-                    f.seek(-1024, os.SEEK_CUR)  # Retrocede el puntero del archivo para volver a enviar el paquete
-                else:
-                    seq_number += 1
 
-        if client_address:
-            self.send_payload(b'END', seq_number, client_address=client_address)
-        else:
-            self.send_payload(b'END', seq_number)
+                self.send_payload(data, seq_number, client_address=client_address)
+
+                ack_received = False
+                while not ack_received:
+                    socket.settimeout(1)
+                    try:
+                        packet = self.receive_packet()[0]
+                        if packet.is_ACK and packet.seq_number == seq_number:
+                            ack_received = True
+                            seq_number += 1
+
+                    except socket.timeout:
+                        f.seek(-1024, os.SEEK_CUR)  # Retrocede el puntero del archivo para volver a enviar el paquete
+                        break
+
+        self.send_payload(b'END', seq_number, client_address=client_address)
+
         print(f"Se envió el archivo {filename} correctamente")
