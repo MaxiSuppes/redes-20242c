f25126c50f235b430bb18d77773029242fc3d7e5
Maxi <maxisuppes@hotmail.com.ar>
2024-09-27 21:36:59 -0300

    Integrado Packet de bauti

--- src/UDPStopAndWait.py
+++ src/UDPStopAndWait.py
@@ -1,6 +1,7 @@
 import queue
-import struct
 import socket
+
+from src.Packet import Packet
 
 PACKET_SIZE = 1024
 PACKET_NUMBER_SIZE = 4
@@ -13,23 +14,17 @@ class UDPStopAndWait:
         self.connection = connection
         self.message_queue = message_queue
 
-    def receive_packet(self):
+    def receive_packet(self, timeout=None):
         if self.message_queue:
-            print(f"Obteniendo mensaje de la Queue")
-            return self.message_queue.get()
+            print(f"Obteniendo mensaje de la Queue con timeout {timeout}")
+            raw_packet = self.message_queue.get(timeout=timeout)
         else:
-            print(f"Obteniendo mensaje del socket")
-            packet, address = self.connection.recvfrom(PACKET_SIZE + PACKET_NUMBER_SIZE)
-            return packet
+            print(f"Obteniendo mensaje del socket con timeout {timeout}")
+            if timeout:
+                self.connection.settimeout(timeout)
+            raw_packet, address = self.connection.recvfrom(PACKET_SIZE + PACKET_NUMBER_SIZE)
 
-    def set_timeout_and_receive_packet(self):
-        if self.message_queue:
-            print(f"Seteando timeout en la queue")
-            return self.message_queue.get(TIMEOUT)
-        else:
-            print(f"Seteando timeout en el socket")
-            self.connection.settimeout(TIMEOUT)
-            return self.receive_packet()
+        return Packet.from_bytes(raw_packet)
 
     def timeout_error_class(self):
         if self.message_queue:
@@ -56,41 +51,42 @@ class UDPStopAndWait:
                     print(f"Enviando paquete {packet_number}", f"{self.external_host_address}")
                     # Se arma el struct para que del otro lado no haya que hacer un .decode() para un pdf
                     # no funca (por ejemplo)
-                    header = struct.pack('>I', packet_number)  # >I empaqueta un unsigned int de 4 bytes en big-endian
-                    packet = header + file_chunk
-                    self.connection.sendto(packet, self.external_host_address)
+                    packet = Packet(packet_number, file_chunk).as_bytes()
+                    self.send_message(packet)
                     try:
-                        packet = self.set_timeout_and_receive_packet()
-                        print(f"Elemento recibido mientras se espera el ack: {packet.decode()}")
-                        if packet.decode() == f'ACK_{packet_number}':
-                            print(f"ACK correcto: {packet.decode()}")
+                        packet = self.receive_packet(timeout=TIMEOUT)
+                        decoded_payload = packet.decoded_payload()
+                        print(f"Elemento recibido mientras se espera el ack: {decoded_payload}")
+                        if packet.is_valid_ack(packet_number):
+                            print(f"ACK correcto: {decoded_payload}")
                             missing_ack = False
                             packet_number += 1
                         else:
-                            print(f"ACK incorrecto: {packet.decode()}")
+                            print(f"ACK incorrecto: {decoded_payload}")
                     except self.timeout_error_class():
                         print(f"Timeout. Reenviando paquete {packet_number}")
 
         print(f"Enviando end")
-        self.send_message(b'END')
+        packet = Packet(packet_number + 1, b"END").as_bytes()
+        self.send_message(packet)
 
     def receive_file(self, file_path):
         with open(file_path, 'wb') as file_to_storage:
             last_packet_received = 0  # El 0 representa que se recibió ningún paquete
             while True:
                 packet = self.receive_packet()
-                if packet == b"END":
+                if packet.payload() == b"END":
                     break
 
-                packet_number = struct.unpack('>I', packet[:4])[0]
-                file_chunk = packet[4:]
+                packet_number = packet.sequence_number()
                 print(f"Paquete recibido: {packet_number}")
-                if int(packet_number) != last_packet_received:
-                    file_to_storage.write(file_chunk)
+                if packet_number != last_packet_received:
+                    file_to_storage.write(packet.payload())
                     last_packet_received = int(packet_number)
                 else:
                     print(f"Paquete {packet_number} ya recibido.")
 
                 print(f"Enviando ACK {packet_number} a {self.external_host_address}.")
-                self.connection.sendto(f'ACK_{packet_number}'.encode(), self.external_host_address)
+                packet = Packet(packet_number, "ACK".encode()).as_bytes()
+                self.send_message(packet)
 
